49304c80278367b3a5c52e2b22eaff71d8d082fa
dngusd <whkim712@gmail.com>
2022-08-17 22:51:19 +0900

    fas

--- register.php
+++ register.php
@@ -19,5 +19,11 @@
       mysqli_query( $jb_conn, $jb_sql_add_user );
       echo "<script>alert('회원가입이 완료되었습니다!');location.href='index.html';</script>"
     } 
+    if ( $wu == 1 ) {
+        echo "<script>alert('사용자이름이 중복되었습니다.');location.href='register.html';</script>";
+      }
+      if ( $wp == 1 ) {
+        echo "<script>alert('비밀번호가 일치하지 않습니다.');location.href='register.html';</script>";
+      }
   }
 ?>
