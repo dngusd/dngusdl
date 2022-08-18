93da99d9f60710105f0e2cf6819ff89e519a9b1d
dngusd <whkim712@gmail.com>
2022-08-18 18:17:06 +0900

    123

--- register.php
+++ register.php
@@ -8,17 +8,17 @@
     $jb_result = mysqli_query( $jb_conn, $jb_sql );
     while ( $jb_row = mysqli_fetch_array( $jb_result ) ) {
       $username_e = $jb_row[ 'id' ];
+        if ( $id == $username_e ) {
+          $wu = 1;
+        } elseif ( $password != $password_confirm ) {
+          $wp = 1;
+        } else {
+          $encrypted_password = password_hash( $password, PASSWORD_DEFAULT);
+          $jb_sql_add_user = "INSERT INTO login ( id, password ) VALUES ( '$username', '$encrypted_password' );";
+          mysqli_query( $jb_conn, $jb_sql_add_user );
+          echo "<script>alert('회원가입이 완료되었습니다!');location.href='index.html';</script>";
+        } 
     }
-    if ( $id == $username_e ) {
-      $wu = 1;
-    } elseif ( $password != $password_confirm ) {
-      $wp = 1;
-    } else {
-      $encrypted_password = password_hash( $password, PASSWORD_DEFAULT);
-      $jb_sql_add_user = "INSERT INTO login ( id, password ) VALUES ( '$username', '$encrypted_password' );";
-      mysqli_query( $jb_conn, $jb_sql_add_user );
-      echo "<script>alert('회원가입이 완료되었습니다!');location.href='index.html';</script>";
-    } 
     if ( $wu == 1 ) {
         echo "<script>alert('사용자이름이 중복되었습니다.');location.href='register.html';</script>";
       }
