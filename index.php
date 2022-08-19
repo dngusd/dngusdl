2e743f733b66331b18d174b40e2114167fe1e23b
dngusd <whkim712@gmail.com>
2022-08-19 17:51:28 +0900

    1414

--- index.php
+++ index.php
@@ -25,15 +25,12 @@
                 </tr>
           </thead>
           <?php
-          // board테이블에서 idx를 기준으로 내림차순해서 10개까지 표시
             $sql = "select * from board order by idx desc limit 0,10"; 
               while($board = mysqli_fetch_array($sql))
               {
-                //title변수에 DB에서 가져온 title을 선택
                 $title=$board["title"]; 
                 if(strlen($title)>30)
                 { 
-                  //title이 30을 넘어서면 ...표시
                   $title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
                 }
           ?>
