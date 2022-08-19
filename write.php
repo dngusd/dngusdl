373f9f7f1db247318bbf8d141d74d34e9737a439
dngusd <whkim712@gmail.com>
2022-08-20 00:21:32 +0900

    dwq

--- write.php
+++ write.php
@@ -39,6 +39,28 @@
                 <a href="#" class="on">등록</a>
                 <a href="#">취소</a>
             </div>
+            <?php
+  if ( isset($_POST['id']) && isset($_POST['password']) && isset($_POST['password_confirm'])) {
+      $username = $_POST['nickname'];
+      $id = $_POST[ 'id' ];
+      $password = $_POST[ 'password' ];
+      $password_confirm = $_POST[ 'password_confirm' ];
+    $jb_conn = mysqli_connect( 'localhost', 'whkim712', 'white.1245', 'dngusdldl' );
+    $jb_sql = "SELECT id FROM login WHERE id = '$id';";}
+    $jb_result = mysqli_query( $jb_conn, $jb_sql );
+    $jb_resultt = mysqli_fetch_array($jb_result);
+    if ( $jb_resultt ) {
+        echo "<script>alert('사용자이름이 중복되었습니다.');location.href='register.html';</script>";
+    } elseif ( $password != $password_confirm ) {
+        echo "<script>alert('비밀번호가 일치하지 않습니다.');location.href='register.html';</script>";
+    } else {
+      $encrypted_password = password_hash( $password, PASSWORD_DEFAULT);
+      $jb_sql_add_user = "INSERT INTO login ( id, password,name ) VALUES ( '{$id}', '{$encrypted_password}', '{$username}');";
+      mysqli_query( $jb_conn, $jb_sql_add_user );
+      echo "<script>alert('회원가입이 완료되었습니다!');location.href='index.php';</script>";
+    } 
+  }
+?>
         </div>
     </div>
 </body>
