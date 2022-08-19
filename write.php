5f090508116042189fe70b7434ee66e90e94f95d
dngusd <whkim712@gmail.com>
2022-08-20 03:41:58 +0900

    1232435

--- write.php
+++ write.php
@@ -1,3 +1,13 @@
 <?php
-    echo "{$_POST['title']} | {$_POST['contents']}";
+    $title = $_POST['title'];
+    $cont = $_POST['contents'];
+    $link = mysqli_connect( 'localhost', 'whkim712', 'white.1245', 'dngusdldl' );
+    $id = $_SESSION['id'];
+    $ququ = "INSERT into board (id, cont, title, date, name) values ('$id', '$cont','$title',now(),(select name from login where id='$id'))";
+    if(mysqli_query($link,$ququ)) {
+        echo "<script>alert('글이 작성되었습니다.');location.href='/';</script>";
+    }
+    else {
+        echo "<script>alert('글 작성에 실패하였습니다.');history.back();</script>";
+    }
 ?>
