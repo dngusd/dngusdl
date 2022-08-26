b8d1cc41e6125b30236881ac4f4248c9334774db
dngusd <whkim712@gmail.com>
2022-08-27 00:33:33 +0900

    13qe2

--- write.php
+++ write.php
@@ -11,7 +11,7 @@
             $uploaded_file_name_tmp = $_FILES[ 'myfile' ][ 'tmp_name' ];
             $uploaded_file_name = $_FILES[ 'myfile' ][ 'name' ];
             $upload_folder = "./uploads";
-            move_uploaded_file( $uploaded_file_name_tmp, $upload_folder . $uploaded_file_name );
+            move_uploaded_file( $uploaded_file_name_tmp, $upload_folder , $uploaded_file_name );
         }
     }
     else {
